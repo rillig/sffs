0de9c6585b0155616975f2e2c369a1f1875c22a7
Roland Illig <roland.illig@gmx.de>
2021-11-21 16:00:44 +0100

    reduce visibility of Storage methods

--- src/main/java/de/roland_illig/sffs/Storage.java
+++ src/main/java/de/roland_illig/sffs/Storage.java
@@ -7,21 +7,21 @@ class Storage implements AutoCloseable {
 
     private final RandomAccessFile file;
 
-    public Storage(RandomAccessFile file) {
+    Storage(RandomAccessFile file) {
         this.file = file;
     }
 
-    public int readIntAt(long pos) throws IOException {
+    int readIntAt(long pos) throws IOException {
         file.seek(pos);
         return file.readInt();
     }
 
-    public long readLongAt(long pos) throws IOException {
+    long readLongAt(long pos) throws IOException {
         file.seek(pos);
         return file.readLong();
     }
 
-    public void readFully(long pos, byte[] buf, int bufOffset, int bufLength) throws IOException {
+    void readFully(long pos, byte[] buf, int bufOffset, int bufLength) throws IOException {
         file.seek(pos);
         file.readFully(buf, bufOffset, bufLength);
     }
