cd44f8c3093b9914c0d236d977cfc361c88018cd
Roland Illig <roland.illig@gmx.de>
2021-11-27 00:07:00 +0100

    demonstrate out of bounds read for large regular files
    Same pattern as for small files, just with an additional seek.

--- src/test/java/de/roland_illig/sffs/internal/RegularFileTest.java
+++ src/test/java/de/roland_illig/sffs/internal/RegularFileTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.io.TempDir;
 class RegularFileTest {
 
     @Test
-    void read_large_buffer(@TempDir File tmpdir) throws IOException {
+    void read_small_file_large_buffer(@TempDir File tmpdir) throws IOException {
         var f = new File(tmpdir, "storage");
 
         try (var fs = new Filesystem(f, "rw")) {
@@ -25,6 +25,27 @@ class RegularFileTest {
                 var buf = new byte[16 * 1024];
                 var n = file.read(buf, 0, buf.length);
                 assertThat(n).isEqualTo(1);
+                assertThat(buf[0]).isEqualTo((byte) 0x55);
+            }
+        }
+    }
+
+    @Test
+    void read_large_file_large_buffer(@TempDir File tmpdir) throws IOException {
+        var f = new File(tmpdir, "storage");
+
+        try (var fs = new Filesystem(f, "rw")) {
+            try (var file = fs.open(Path.of("small"), "w")) {
+                var buf = new byte[]{0x55};
+                file.seek(100_000);
+                file.write(buf, 0, 1);
+            }
+
+            try (var file = fs.open(Path.of("small"), "r")) {
+                var buf = new byte[16 * 1024];
+                file.seek(100_000);
+                var n = file.read(buf, 0, buf.length);
+                assertThat(n).isEqualTo(2400); // FIXME: must be 1
                 assertThat(buf[0]).isEqualTo((byte) 0x55);
             }
         }
