9369160383181cd41c1f4da4c408026fb656b4e4
Roland Illig <roland.illig@gmx.de>
2021-11-26 23:34:09 +0100

    add verification state to real world test of the filesystem
    Verification fails with an IndexOutOfBoundsException for .gitignore, not
    sure why yet since that file should be pretty short.

--- src/test/java/de/roland_illig/sffs/FilesystemRealWorldTest.java
+++ src/test/java/de/roland_illig/sffs/FilesystemRealWorldTest.java
@@ -1,5 +1,8 @@
 package de.roland_illig.sffs;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -8,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -19,6 +23,13 @@ class FilesystemRealWorldTest {
 
         try (var fs = Filesystem.open(storage, "rw")) {
             Files.walkFileTree(Path.of("."), new CopyIn(fs));
+        }
+
+        try (var fs = Filesystem.open(storage, "rw")) {
+            assertThatThrownBy(() -> Files.walkFileTree(Path.of("."), new Verify(fs)))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasCauseInstanceOf(IndexOutOfBoundsException.class);
+            // .gitignore: Index out of range: 4120
         }
     }
 
@@ -57,6 +68,52 @@ class FilesystemRealWorldTest {
                 }
             }
             return FileVisitResult.CONTINUE;
+        }
+    }
+
+    static class Verify extends SimpleFileVisitor<Path> {
+        private final Filesystem fs;
+        private final byte[] outerBuf = new byte[4096];
+        private final byte[] innerBuf = new byte[4096];
+
+        Verify(Filesystem fs) {
+            this.fs = fs;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            // skip hidden files, especially .git, since that might contain files over 2 MB.
+            var name = dir.normalize().getFileName().toString();
+            if (name.equals(""))
+                return FileVisitResult.CONTINUE;
+            if (name.startsWith("."))
+                return FileVisitResult.SKIP_SUBTREE;
+
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            assertFilesEqual(file.normalize());
+            return FileVisitResult.CONTINUE;
+        }
+
+        private void assertFilesEqual(Path file) throws IOException {
+            try (var outer = new FileInputStream(file.toFile())) {
+                try (var inner = fs.open(file, "r")) {
+                    while (true) {
+                        int nOuter, nInner;
+                        nOuter = outer.read(outerBuf, 0, outerBuf.length);
+                        nInner = inner.read(innerBuf, 0, innerBuf.length);
+                        assertThat(nInner).isEqualTo(nOuter);
+                        assertThat(Arrays.copyOf(outerBuf, nOuter)).isEqualTo(Arrays.copyOf(innerBuf, nInner));
+                        if (nOuter == 0)
+                            break;
+                    }
+                }
+            } catch (IndexOutOfBoundsException e) {
+                throw new IllegalStateException(file.toString(), e);
+            }
         }
     }
 }
