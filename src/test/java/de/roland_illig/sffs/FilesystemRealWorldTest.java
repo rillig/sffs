da2176095080987a0f9010f818df6ae636cdf297
Roland Illig <roland.illig@gmx.de>
2021-11-26 23:15:32 +0100

    add comprehensive test for using the filesystem

---
+++ src/test/java/de/roland_illig/sffs/FilesystemRealWorldTest.java
@@ -0,0 +1,68 @@
+package de.roland_illig.sffs;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class FilesystemRealWorldTest {
+
+    @Test
+    void copyTrees(@TempDir File tmpdir) throws IOException {
+        var storage = new File(tmpdir, "storage");
+
+        try (var fs = Filesystem.open(storage, "rw")) {
+            assertThatThrownBy(() -> Files.walkFileTree(Path.of("."), new CopyIn(fs)))
+                    .isInstanceOf(FileSystemException.class)
+                    .hasMessageEndingWith("expected DIRECTORY, got REGULAR");
+            // see Directory.enlarge
+        }
+    }
+
+    static class CopyIn extends SimpleFileVisitor<Path> {
+        private final Filesystem fs;
+        private final byte[] buf = new byte[4096];
+
+        CopyIn(Filesystem fs) {
+            this.fs = fs;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            dir = dir.normalize();
+
+            // skip hidden files, especially .git, since that might contain files over 2 MB.
+            var name = dir.getFileName().toString();
+            if (name.equals(""))
+                return FileVisitResult.CONTINUE;
+            if (name.startsWith("."))
+                return FileVisitResult.SKIP_SUBTREE;
+
+            fs.mkdir(dir.normalize());
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            file = file.normalize();
+
+            try (var in = new FileInputStream(file.toFile())) {
+                try (var out = fs.open(file, "w")) {
+                    int n;
+                    while ((n = in.read(buf, 0, buf.length)) > 0)
+                        out.write(buf, 0, n);
+                }
+            }
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
